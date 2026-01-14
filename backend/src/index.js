11d7a5e0c0f60d4e24f2a69bbab721f0c26a5c52
Sujit Yadav <yadavsujit3366@gmail.com>
2026-01-14 20:44:20 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://mentormatch-s8vm.vercel.app"],
+    origin: ["https://mentormatch-s8vm.vercel.app"],
     credentials: true,
   })
 );
